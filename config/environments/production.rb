84492db35460379d3da0bed2ebe7b2e881c135c8
vicnoob <vic.geniusgaming@gmail.com>
2016-07-11 15:48:29 +0700

    Add account activation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,17 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.force_ssl = true
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  host = 'floating-dawn-60803.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 '587',
+    domain:               'heroku.com',
+    user_name:            'vicnoob.gg',
+    password:             'vicnoob123',
+    authentication:       'plain',
+    enable_starttls_auto: true  }
 end
